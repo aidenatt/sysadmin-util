2c38cd1c7e3a7d7f71736ad28d51749024bdd204
Aiden Attenborough <aiden@aadev.com.au>
2018-08-11 03:59:05 +0930

    Added basic fan speed script - Still WIP

---
+++ servers/r810/fanspeed.sh
@@ -0,0 +1,22 @@
+#1 = 0x01
+#100 = 0x64
+
+if [ -z "$2" ] && [ -z "$3" ] ; then
+    username=root
+    password=calvin
+else
+    username=$2
+    password=$3
+fi
+
+if [ $1 -lt 100 -a $1 -gt 0 ] ; then
+    speed=$(eval printf '%X' $1) # Convert input to hex
+    errormessage=$( ipmitool -I lanplus -H 192.168.1.180 -U $username -P $password raw 0x30 0x30 0x02 0xff 0x$speed 2>&1)
+    if [[ $errormessage = *"Error:"* ]] ; then
+        echo "Credentials Incorrect. Fan speed not set."
+    else
+        echo "R810 fan speed set to $1%"
+    fi
+else
+    echo "Value must be between 1 & 100."
+fi
